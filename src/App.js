4430713e5a83db69811d00a94902edfe7f4e94c7
alireza vafadar <alirezavafadar.ca@gmail.com>
2022-08-28 15:30:30 -0400

    clock fixed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import Clock from './Clock';
+import timezone from './timezone.json';
 import './App.css';
 import { Button } from 'react-bootstrap';
 
-function App() {
+function App(props) {
+  
+  const [clocks, setClocks] = useState([]); 
+  const [selectedClock, setSelectedClock] = useState('Asia/Tehran');
+
+  const handleChange = e => {
+    setSelectedClock(e.target.value);
+  }
+
+  const addTimeZone=()=> {
+    if (clocks.findIndex(c => c.Timezone == selectedClock) < 0) {
+        const zone = timezone.find(k => k.Timezone == selectedClock);
+        setClocks(prevState => [...prevState, zone] );
+    }
+  }
+
+  const removeClick=(zone)=>
+    {
+      let updateClocks = clocks;
+      let index = updateClocks.findIndex(t => t.Timezone === zone);
+      updateClocks.splice(index,1)
+      setClocks(updateClocks);
+    }
+
+    const optionItems = timezone.map((zone) =>
+    <option value={zone.Timezone} key={zone.Timezone} onChange={handleChange}>{zone.Country}-{zone.Timezone}</option>
+    );
+
+    const showClocks = clocks.map((zone) =>
+        <Clock {...zone} key={zone.Timezone} removeClick={()=>removeClick(zone.Timezone)} />
+    );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="">
+                <div>
+                    <p> 
+                        <select className="" value={selectedClock} onChange={handleChange} >
+                            {optionItems}
+                        </select>
+                    </p>
+                    <p>
+                        <button onClick={addTimeZone} className="">ADD CLOCK </button>
+                    </p>
+                </div>
+                {showClocks}
+            </div>
+
   );
 }
 
---
+++ src/Clock.js
@@ -0,0 +1,46 @@
+import React, {useEffect, useState} from 'react';
+import { Button } from 'react-bootstrap';
+
+function Clock(props) {
+  
+  const [time, setTime] = useState(null); 
+
+  useEffect(() => {
+    setInterval(() => {
+      tick();
+    }, 1000);
+  },[]);
+
+  const tick=()=> {
+    setTime(getCurrentTime);
+  } 
+  const getCurrentTime =()=> {
+    const today = new Date();
+    today.setMinutes(today.getMinutes() + today.getTimezoneOffset() + parseInt(props.Offset));
+    return today.toLocaleString();
+  }
+
+  return (
+    <div className=''>       
+      <div className=''>            
+      <div className=''>                    
+        <header>
+        <div onClick={props.removeClick} className=''>×</div>
+        <h5>
+        {props.Country}        
+        </h5>        
+        </header>                           
+      </div>                  
+      <div className=''>
+          <p>{time}</p>
+          <img height="60px" src={'https://flagicons.lipis.dev/flags/1x1/' + props.ISO.toLowerCase() + '.svg' } />
+      </div>
+      <footer className=''>
+          <h5>{props.Timezone}</h5>                            
+        </footer>
+      </div>
+      </div>
+  );
+}
+
+export default Clock;
